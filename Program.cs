ca09caee91cac64def0b3cac73e0f0bb42790150
Andreas Nygård <nygardandreas@hotmail.com>
2024-10-17 16:44:04 +0200

    Created main menu in Program class

--- Program.cs
+++ Program.cs
@@ -2,6 +2,48 @@
 {
     static void Main()
     {
-        
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            System.Console.WriteLine("---- Welcome to ----");
+            System.Console.WriteLine("T H E  G U E S T B O O K");
+            System.Console.WriteLine();
+            System.Console.WriteLine("1. Login");
+            System.Console.WriteLine("2. Register");
+            System.Console.WriteLine("3. View all members");
+            System.Console.WriteLine("4. About us");
+            System.Console.WriteLine();
+            System.Console.WriteLine("0. Exit program");
+            System.Console.WriteLine();
+
+            System.Console.Write("Write here: ");
+            string? userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    //Login();
+                    break;
+                case "2":
+                    //Register();
+                    break;
+                case "3":
+                    //ViewAllMembers()
+                    break;
+                case "4":
+                    //AboutUs();
+                    break;
+                case "0":
+                    isRunning = false;
+                    Console.Clear();
+                    System.Console.WriteLine("Exit program...");
+                    break;
+                default:
+                    Console.Clear();
+                    System.Console.WriteLine("Invalid input...");
+                    break;
+            }
+        }
     }
 }
