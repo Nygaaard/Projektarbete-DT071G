160f06fd63f7e9e939e01ee446a86b3f2fcaebfe
Andreas Nygård <nygardandreas@hotmail.com>
2024-10-17 16:18:25 +0200

    Logic in Post class

--- Post.cs
+++ Post.cs
@@ -1,4 +1,22 @@
 class Post
 {
-    
+    private string? message;
+    public Post(string m)
+    {
+        Message = m;
+    }
+    public string Message
+    {
+        get => message!;
+        set 
+        {
+            if(String.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Message field can not be empty...");
+            } else
+            {
+                message = value;
+            }
+        }
+    }
 }
